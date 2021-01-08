819d0e491be8bcb6034798e205b1ca651a711fa1
nnk-ymmt <garden.n1234567890@gmail.com>
2021-01-08 20:28:28 +0900

    initial commit

---
+++ Part13-tableView-Tests/Part13_tableView_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Part13_tableView_Tests.swift
+//  Part13-tableView-Tests
+//
+//  Created by 山本ののか on 2021/01/08.
+//
+
+import XCTest
+@testable import Part13_tableView_
+
+class Part13_tableView_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
