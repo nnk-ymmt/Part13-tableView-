31f48f6456549de8db7dfba8c15922975a22cb2d
nnk-ymmt <garden.n1234567890@gmail.com>
2021-01-10 22:53:17 +0900

    VC更新

--- Part13-tableView-/ViewController.swift
+++ Part13-tableView-/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.register(TableViewCell.loadNib(), forCellReuseIdentifier: TableViewCell.reuseIdentifier)
+            tableView.isHidden = true
+        }
+    }
+
+    enum Fruit {
+        case apple
+        case orange
+        case banana
+        case pineapple
+
+        var stringName: String {
+            switch self {
+            case .apple: return "りんご"
+            case .orange: return "みかん"
+            case .banana: return "バナナ"
+            case .pineapple: return "パイナップル"
+            }
+        }
+
+        var check: Bool {
+            switch self {
+            case .apple: return false
+            case .orange: return true
+            case .banana: return false
+            case .pineapple: return true
+            }
+        }
+    }
+
+    let fruits: [Fruit] = [Fruit.apple, Fruit.orange, Fruit.banana, Fruit.pineapple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.rowHeight = 50
+        tableView.isHidden = false
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else {
+            return UITableViewCell()
+        }
+        let fruit = fruits[indexPath.item]
+        cell.configure(name: fruit.stringName, check: fruit.check)
+        return cell
+    }
+}
