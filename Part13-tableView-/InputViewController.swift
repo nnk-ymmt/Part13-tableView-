7f32b4979463f8e9f8e186695119af046094dce1
nnk-ymmt <garden.n1234567890@gmail.com>
2021-01-12 22:46:29 +0900

    InputVC追加

---
+++ Part13-tableView-/InputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InputViewController.swift
+//  Part13-tableView-
+//
+//  Created by 山本ののか on 2021/01/12.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
